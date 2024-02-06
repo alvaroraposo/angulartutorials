b1b35f0d3a1a51746ab2cf90f71db24cd59963e1
Alvaro Raposo <alvaroraposo@yahoo.com.br>
2024-02-06 15:56:15 -0400

    ngIf, ngFor, ngSwitch

---
+++ 2.StructureDirectives/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Structure Directives';
+  isLoggedIn: boolean = false;
+  username: string = "Umair"
+  names: string[] = ['umair', 'ali', 'john']
+  grade: string = 'A'
+}
